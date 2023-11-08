56bfc248feedd296e771e9f1167d0a67214a2202
Augusto.L <augustolorenzo9@gmail.com>
2023-11-08 19:03:10 -0300

    update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 const mainRoutes = require('./src/routes/mainRoutes.js')
 
 app.use(express.static('public'));
-app.use("/", mainRoutes);
+//app.use("/", mainRoutes);
 
 app.listen(port, () => {
  console.log(`Example app listening at http://localhost:${port}`);
