fdf8c3da419d76de11647756c9dab2bf4379ba33
Augusto.L <augustolorenzo9@gmail.com>
2023-12-10 22:00:41 -0300

    Actualizacion feature/homepage-style

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const controller=require('../controllers/main.controller')
-router.get('/   ', controller.home );
+const controller=require('../controllers/main.controller');
+router.get('/', controller.home );
 router.get('/home', controller.home );
 router.get('/contact', controller.contact);
 router.get('/about', controller.about);
